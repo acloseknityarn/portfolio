7ebc1346181d75536d024c0584d63991818b7e71
Aryamann Verma <avverma19699@gmail.com>
2024-06-21 22:07:10 -0300

    Restructured images folder usage Added images in src folder Changed schema collection to use Image astro component

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,15 +3,27 @@ import { defineCollection, z } from 'astro:content';
 export const collections = {
 	post: defineCollection({
 		type: 'content',
-		schema: z.object({
-			title: z.string(),
-			description: z.string(),
-			publishDate: z.coerce.date(),
-			tags: z.array(z.string()),
-			img: z.object({
-				src: z.string(),
-				alt: z.string()
-			}).optional()
-		}),
+		schema: ( {image} ) => {
+			return z.object({
+				title: z.string(),
+				description: z.string(),
+				publishDate: z.coerce.date(),
+				tags: z.array(z.string()),
+				img: z.object({
+					src: image(),
+					alt: z.string()
+				}).optional()
+			})
+		}
 	}),
 };
+
+const blogCollection = defineCollection({
+	schema: ({ image }) => z.object({
+	  title: z.string(),
+	  cover: image().refine((img) => img.width >= 1080, {
+		message: "Cover image must be at least 1080 pixels wide!",
+	  }),
+	  coverAlt: z.string(),
+	}),
+  });
--- src/content/post/bloom-box.md
+++ src/content/post/bloom-box.md
@@ -2,7 +2,7 @@
 title: Bloom Box
 publishDate: 2019-12-01 00:00:00
 img:
-  src: /assets/stock-2.jpg
+  src: ../../images/stock-2.jpg
   alt: A bright pink sheet of paper used to wrap flowers curves in front of rich blue background
 description: |
   We paired with a cutting-edge music API and a team of horticulturalists
